d26c56826bccea7326516c4cfb6903c272358de6
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2024-04-18 13:21:41 +0530

    Add config.json file and update LinkGenerator.js to use params from config

--- src/LinkGenerator.js
+++ src/LinkGenerator.js
@@ -1,9 +1,15 @@
-const { encode } = require("url-encode-decode");
+const params  = require("../config.json")
 
 function generateBitbucketURL(params) {
   const baseURL =
     "https://bitbucket.org/!api/internal/workspaces/listed-inc/pullrequests/";
   const { fields, page, pagelen, state, projectUUID, accountID } = params;
+
+  if (!fields || !page || !pagelen || !state || !projectUUID || !accountID) {
+    console.error("ERROR: Missing required params");
+    // exit the process
+    process.exit(1);
+  }
 
   const fieldsParam = `fields=${fields.join(",")}`;
   const pageParam = `page=${page}`;
@@ -16,21 +22,6 @@ function generateBitbucketURL(params) {
   return generatedURL;
 }
 
-// Example usage:
-const params = {
-  fields: [
-    "values.title",
-    "values.destination.branch.name",
-    "values.destination.repository.name",
-    "values.closed_on",
-    "values.links.html.href",
-  ],
-  page: 1,
-  pagelen: 70,
-  state: "MERGED",
-  projectUUID: "4642198d-cb4d-4d7e-99cf-bd9ed9872dbd",
-  accountID: "712020:fd66d7a9-e1ce-478d-9e34-9d0f7e1f212c",
-};
 
 const generatedURL = generateBitbucketURL(params);
 console.log(generatedURL);
