a3f314f51bcac6284506e3f12d2a7ec35ad0ed99
Vishal Kumar <25267239+vishalx360@users.noreply.github.com>
2024-04-19 13:24:51 +0530

    Refactor index.js and utils.js to improve data transformation and concatenation logic

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@ const data = require("../input.json");
 const fs = require("fs");
 const converter = require("json-2-csv");
 const path = require("path");
-const { transformer } = require("./utils");
+const { transformer, concatDataByDate } = require("./utils");
 
 async function main() {
   console.log("Transforming Data...");
   const transformedData = transformer(data);
   
+  const newData = {}
+  transformedData.forEach((item) => {
+    const date = item.Date;
+    if (!newData[date]) {
+      newData[date] = [];
+    }
+    newData[date].push(item);
+  });
+
+  const finalData =  concatDataByDate(newData);
+
+ 
   // const jsonFilePath = path.join(__dirname, "../output/transformedData.json");
   // console.log(`Generating JSON file at ${jsonFilePath}`);
   // fs.writeFileSync(jsonFilePath, JSON.stringify(transformedData, null, 2));
   
-  const csv = await converter.json2csv(transformedData);
+  const csv = await converter.json2csv(finalData);
   const csvFilePath = path.join(__dirname, "../output.csv");
   console.log(`Generating CSV file at ${csvFilePath}`);
   fs.writeFileSync(csvFilePath, csv);
--- src/utils.js
+++ src/utils.js
@@ -14,4 +14,33 @@ function convertObject(inputObj) {
   };
 }
 
-module.exports = { transformer, convertObject };
+function concatDataByDate(data) {
+  const concatenatedData = [];
+  Object.keys(data).forEach((date) => {
+      data[date].forEach((item) => {
+          const title = item.Title;
+          const description = item['Long Description'];
+          const reference = item.Reference;
+      
+          const existingItem = concatenatedData.find((item) => item.Date === date);
+      
+          if (existingItem) {
+              existingItem.Title = `${existingItem.Title}, ${title}`;
+              existingItem['Long Description'] = `${existingItem['Long Description']}, ${description}`;
+              existingItem.Reference = `${existingItem.Reference}, ${reference}`;
+          } else {
+              concatenatedData.push({
+                  Date: date,
+                  Title: title,
+                  'Long Description': description,
+                  Reference: reference
+              });
+          }
+      });
+  });
+
+  return concatenatedData;
+}
+
+
+module.exports = { transformer, convertObject, concatDataByDate };
